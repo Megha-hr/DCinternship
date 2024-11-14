929dcb2ff8bf47a76f54cd4fd0fbd4909d053197
Megha <123megharavindra@gmail.com>
2024-11-14 18:34:26 +0530

    proshop v2 ecommerce project as week 8 task

---
+++ proshop-v2/frontend/src/components/Header.jsx
@@ -0,0 +1,33 @@
+import { Navbar, Nav, Container, NavDropdown, Badge } from 'react-bootstrap';
+import { FaShoppingCart, FaUser } from 'react-icons/fa'
+import {LinkContainer} from "react-router-bootstrap"
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
+        <Container>
+          <LinkContainer to="/">
+          <Navbar.Brand >ProShop</Navbar.Brand>
+          </LinkContainer>
+          
+            <Navbar.Toggle aria-controls='basic-navbar-nav' />
+            <Navbar.Collapse id='basic-navbar-nav'>
+                <Nav className="ms-auto">
+                  <LinkContainer to="/">
+                  <Nav.Link><FaShoppingCart />cart</Nav.Link>
+                  </LinkContainer>
+                  <LinkContainer to="/">
+                  <Nav.Link><FaUser />Sign in</Nav.Link>
+                  </LinkContainer>
+                   
+                   
+                </Nav>
+                </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  )
+}
+
+export default Header
